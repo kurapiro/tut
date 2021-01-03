baf0c65d7278fe7fbf65c72e3ad3219e76fcc2f7
kurapiro <bpylori918@gmail.com>
2021-01-03 23:37:30 +0900

    TypeErrorを解決

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ if __name__ == "__main__":
     print("Hello Python!")
     i = 1
     j = 2
-    print("i + j = " + (i + j))
-    print("i - j = " + (i - j))
-    print("i * j = " + (i * j))
-    print("i / j = " + (i / j))
+    print("i + j = " + (str)(i + j))
+    print("i - j = " + (str)(i - j))
+    print("i * j = " + (str)(i * j))
+    print("i / j = " + (str)(i / j))
     print("finish")
